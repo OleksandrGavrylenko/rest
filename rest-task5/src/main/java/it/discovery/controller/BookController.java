6878e46a4c875f2f09e265634ae7d7c4cb58a5d1
Sergey <sergey.morenets@yandex.ua>
2018-09-29 15:15:38 +0300

    Task #5

--- rest-task5/src/main/java/it/discovery/controller/BookController.java
+++ rest-task5/src/main/java/it/discovery/controller/BookController.java
@@ -1,11 +1,25 @@
 package it.discovery.controller;
 
+import it.discovery.model.Book;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/book")
-public class BookController {	
+public class BookController {
+
+    @GetMapping(value = "sample", produces = {
+            MediaType.APPLICATION_JSON_UTF8_VALUE,
+            MediaType.APPLICATION_XML_VALUE
+    })
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Book sample() {
+        return new Book();
+    }
 
 
 }
